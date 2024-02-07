bc0ac0d1b46b3a0b3269a971550fcc106c5e5efd
zakariasalhi12 <rafayelle2005@gmail.com>
2024-02-07 19:58:07 +0100

    atoi

--- basicatoi2.go
+++ basicatoi2.go
@@ -1,10 +1,12 @@
 package piscine
 
-func BasicAtoi(s string) int {
+func BasicAtoi2(s string) int {
 	res := 0
 	for _, char := range s {
 		if char >= '0' && char <= '9' {
 			res = res*10 + int(char-48)
+		} else {
+			return 0
 		}
 	}
 	return res
