1cff56d5e41a52c1d8bcdbd2f879907b64f37afb
zakariasalhi12 <rafayelle2005@gmail.com>
2024-02-07 17:40:20 +0100

    upd

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	for char := 'a'; char < 'z'; char++ {
+	for char := 'a'; char <= 'z'; char++ {
 		z01.PrintRune(char)
 	}
 	z01.PrintRune(10)
