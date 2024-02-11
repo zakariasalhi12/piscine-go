3691c22a676862a75ceeec4a47e9afadd45631f1
zakariasalhi12 <rafayelle2005@gmail.com>
2024-02-11 19:53:35 +0100

    converbase

--- convertbase.go
+++ convertbase.go
@@ -1,37 +1,28 @@
 package piscine
 
 func ConvertBase(nbr, baseFrom, baseTo string) string {
-	res := ""
-	b := AtoiBase(nbr, baseFrom)
-
+	res := 0
+	index := 0
 	if !isvalidebase(baseFrom) || !isvalidebase(baseTo) {
-		return "NV"
-	}
-	res = PrintNbrBase2(b, baseTo)
-	return res
-}
-
-func PrintNbrBase2(nbr int, base string) string {
-	res := ""
-	negative := false
-
-	if nbr == 0 {
-		return "0"
+		return ""
 	}
 
-	if nbr < 0 {
-		negative = true
-		nbr = -nbr
+	for i := range nbr {
+		for j := range baseFrom {
+			if nbr[i] == baseFrom[j] {
+				index = j
+				break
+			}
+		}
+		res = res*len(baseFrom) + index
 	}
 
-	for nbr != 0 {
-		res = string(base[nbr%len(base)]) + res
-		nbr /= len(base)
+	reseul := ""
+	for res != 0 {
+		digit := res % len(baseTo)
+		reseul = string(baseTo[digit]) + reseul
+		res /= len(baseTo)
 	}
 
-	if negative {
-		res = "-" + res
-	}
-
-	return res
+	return reseul
 }
